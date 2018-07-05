4320c72e8f4bb7da84fe6220c3264449c3caf7b0
Cameron Eldridge <cameldridge+git@gmail.com>
2018-07-05 11:22:40 -0400

    Add proper documentation

--- PrettyString/Classes/String+Pretty.swift
+++ PrettyString/Classes/String+Pretty.swift
@@ -9,7 +9,11 @@
 import Foundation
 
 extension String {
-    /// prettifies
+    /// Prettifies the string in much the same way that `PrettyString.parse` would
+    ///
+    /// - Parameter config: The config to use to style this string;
+    /// - Returns: The attributed string
+    /// - Throws: A `PrettyString.Error` that describes what went wrong
     public func prettify(_ config: PrettyString.Config = .default) throws -> NSAttributedString {
         return try PrettyString(self, config: config).parse()
     }
